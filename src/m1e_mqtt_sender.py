1f096fbf1e159cda01da006ea1c9c7a6d17e0b38
chenx16 <chenx16@rose-hulman.edu>
2019-02-06 13:54:12 -0500

    Done(Xinlai Chen)

--- src/m1e_mqtt_sender.py
+++ src/m1e_mqtt_sender.py
@@ -5,11 +5,11 @@ import time
 
 
 def main():
-    name1 = input("Enter one name (subscriber): ")
+    name1 = input("Enter one name (subscriber): ") # String
     name2 = input("Enter another name (publisher): ")
 
-    mqtt_client = com.MqttClient()
-    mqtt_client.connect(name1, name2)
+    mqtt_client = com.MqttClient() # Calls Init
+    mqtt_client.connect(name1, name2) # Message call 2 things
     time.sleep(1)  # Time to allow the MQTT setup.
     print()
 
